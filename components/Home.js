9f5e806e219b6a821bc6fd1f504e280ef7be7c22
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-02 10:45:02 -0800

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,21 @@
+import { useState, useContext } from "react";
+import { KanjisContext } from "../App";
+import Write from "./Write.js";
+import Quiz from "./Quiz.js";
+import Practice from "./Practice.js";
+
 export default Home = () => {
+  const [currPage, setCurrPage] = useState(<Quiz />);
+  //const kanjis = useContext(KanjisContext);
+  const page = () => {
+    alert("page");
+    setCurrPage(<Write />);
+  };
   return (
     <div>
       <p>kanji page</p>
+      {currPage}
+      <button onClick={() => page()}>Write</button>
     </div>
   );
 };
