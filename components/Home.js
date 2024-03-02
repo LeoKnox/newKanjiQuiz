54f61cf2e010d76e51a89bcee30418d0ed3d153c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-02 10:50:45 -0800

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -3,15 +3,19 @@ import { KanjisContext } from "../App";
 import Write from "./Write.js";
 import Quiz from "./Quiz.js";
 import Practice from "./Practice.js";
+import Manage from "./Manage.js";
 
 export default Home = () => {
-  const [currPage, setCurrPage] = useState(<Quiz />);
+  const [currPage, setCurrPage] = useState(<Manage />);
   //const kanjis = useContext(KanjisContext);
   return (
     <div>
       <p>kanji page</p>
       {currPage}
+      <button onClick={() => setCurrPage(<Manage />)}>Home</button>
       <button onClick={() => setCurrPage(<Write />)}>Write</button>
+      <button onClick={() => setCurrPage(<Quiz />)}>Quiz</button>
+      <button onClick={() => setCurrPage(<Practice />)}>Practice</button>
     </div>
   );
 };
