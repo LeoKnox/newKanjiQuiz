44abd7a916b6c166898ac02f16505d1ff2c8a32c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-02 18:16:07 -0800

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,32 @@ export default Home = () => {
   const kanjis = useContext(AllContext);
   const [kc, setKc] = useState(KanjisContext);
   return (
+    <div>import { useState, useContext } from "react";
+import KanjisContext from "../App";
+import AllContext from "./kanji-context.js";
+import Write from "./Write.js";
+import Quiz from "./Quiz.js";
+import Practice from "./Practice.js";
+import Manage from "./Manage.js";
+
+export default Home = () => {
+  const [currPage, setCurrPage] = useState(<Manage />);
+  const kanjis = useContext(AllContext);
+  const [kc, setKc] = useState(KanjisContext);
+  console.log(kc);
+  return (
     <div>
+      <p>kanji page</p>
+      <p>{kanjis[0]["kanji"]}</p>
+      {currPage}
+      <button onClick={() => setCurrPage(<Manage />)}>Home</button>
+      <button onClick={() => setCurrPage(<Write />)}>Write</button>
+      <button onClick={() => setCurrPage(<Quiz />)}>Quiz</button>
+      <button onClick={() => setCurrPage(<Practice />)}>Practice</button>
+    </div>
+  );
+};
+
       <p>kanji page</p>
       <p>{kanjis[0]["kanji"]}</p>
       <p>{kc[0]["id"]}</p>
