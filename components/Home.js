bf8b784cfef8a378e4d2ab1b95c1be00a313b00f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-03 11:46:38 -0800

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import KanjisContext from "../App";
 import AllContext from "./kanji-context.js";
+import findPos from "./kanji-context.js";
 import Write from "./Write.js";
 import Quiz from "./Quiz.js";
 import Practice from "./Practice.js";
@@ -9,15 +10,15 @@ import Manage from "./Manage.js";
 export default Home = () => {
   const [currPage, setCurrPage] = useState(<Manage />);
   const kanjis = useContext(AllContext);
-  const [kc, setKc] = useState(AllContext);
+  const [kc, setKc] = useState(KanjisContext);
   const kd = useContext(KanjisContext);
-  console.log(kd);
+  console.log(AllContext);
   return (
     <div>
       <p>kanji page</p>
       <p>{kanjis[0]["kanji"]}</p>
       {currPage}
-      <p>{kc[0]["kanji"]}</p>
+      <p>{kc.props.value[0]["kanji"]}</p>
       <button onClick={() => setCurrPage(<Manage />)}>Home</button>
       <button onClick={() => setCurrPage(<Write />)}>Write</button>
       <button onClick={() => setCurrPage(<Quiz />)}>Quiz</button>
