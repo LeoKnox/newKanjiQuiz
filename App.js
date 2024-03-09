0b7391aaf2e83777a3ff3c4fcb6cbf738a116ef1
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-08 20:17:44 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
