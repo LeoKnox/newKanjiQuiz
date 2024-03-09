5b31c646e8d3420b9a9dac27dbe8e6582b9f95a0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-09 15:17:41 -0800

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -2,13 +2,25 @@ import { useState } from "react";
 import { KanjiContext } from "../KanjiContext.js";
 import DashBoard from "./DashBoard.js";
 import AllKanji from "./AllKanji.js";
+import Quiz from "./Quiz.js";
 
 export default Home = () => {
   const [componentPage, setComponentPage] = useState(<DashBoard />);
+  const kanjiData = [
+    { word: "ichi", meaning: "one" },
+    { word: "ni", meaning: "two" },
+    { word: "san", meaning: "three" },
+    { word: "yon", meaning: "four" },
+    { word: "go", meaning: "five" },
+    { word: "roku", meaning: "six" },
+  ];
   return (
     <div>
       <button onClick={() => setComponentPage(<DashBoard />)}>Home</button>
       <button onClick={() => setComponentPage(<AllKanji />)}>All Kanji</button>
+      <button onClick={() => setComponentPage(<Quiz kanjiData={kanjiData} />)}>
+        Quiz
+      </button>
       <p>{componentPage}</p>
     </div>
   );
