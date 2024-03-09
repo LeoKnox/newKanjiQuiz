de0f73a1756a934dc9a722eaee820e3035909779
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-08 20:15:51 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { KanjiContext } from "./KanjiContext";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-const allKanji = "all kanji";
 
-root.render(
-  <KanjiContext.Provider value={allKanji}>
-    <App />
-  </KanjiContext.Provider>
-);
+root.render(<App />);
