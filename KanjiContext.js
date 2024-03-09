768ee607ed90305baf337a752019feff852967e7
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-08 19:46:08 -0800

    Update KanjiContext.js

--- KanjiContext.js
+++ KanjiContext.js
@@ -2,23 +2,13 @@ import { createContext, useState } from "react";
 
 export const MyKanjis = createContext();
 
-const KanjiContext = ({
-  kanjiData = [
-    {
-      id: 103,
-      kanji: "同",
-      stroke: 6,
-      meaning: "same",
-      on: "dō",
-      kun: "ona-ji",
-    },
-  ],
-}) => {
+const KanjiContext = ({ kanjiData }) => {
   const [storeKanjis, setStoreKanjis] = useState([]);
-  return;
-  <MyKanjis.Provider value={{ storeKanjis, setStoreKanjis }}>
-    {kanjiData}
-  </MyKanjis.Provider>;
+  return (
+    <MyKanjis.Provider value={{ storeKanjis, setStoreKanjis }}>
+      {kanjiData}
+    </MyKanjis.Provider>
+  );
 };
 
 export default KanjiContext;
