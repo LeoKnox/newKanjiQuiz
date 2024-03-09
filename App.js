44078af98f82910780b05fab788eb487eadd09e0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-09 15:08:38 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import "./styles.css";
-import { KanjiContext } from "./KanjiContext.js";
 import Home from "./components/Home.js";
 
 export default function App() {
   const value = "kanji data";
   return (
     <div className="App">
-      <KanjiContext.Provider value={value}>
-        <Home />
-      </KanjiContext.Provider>
+      <Home />
     </div>
   );
 }
