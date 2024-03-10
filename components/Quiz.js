901dc97ed2068b93c4d68f0350277b66cf0257a6
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-10 11:23:24 -0700

    Update Quiz.js

--- components/Quiz.js
+++ components/Quiz.js
@@ -9,10 +9,12 @@ export default Quiz = ({ kanjiData }) => {
     setQuizes(selected);
     setAnswer(randAnswer.meaning);
   }, []);
-  const confirmAnswer = (answer) => {
-    let obj = quizes.find((o) => o.meaning === answer);
-    console.log(`answer: ${answer} : obj: ${obj}`);
-    alert(obj.meaning);
+  const confirmAnswer = (selection) => {
+    if (selection === answer) {
+      alert("correct");
+    } else {
+      alert("incorrect");
+    }
   };
   return (
     <div>
