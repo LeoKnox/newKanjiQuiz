f887eaa5644eb1aab2996575264a61ca30720cc6
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-10 11:51:58 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -3,6 +3,7 @@ import { KanjiContext } from "../KanjiContext.js";
 import DashBoard from "./DashBoard.js";
 import AllKanji from "./AllKanji.js";
 import Quiz from "./Quiz.js";
+import Practice from "./Practice.js";
 
 export default Home = () => {
   const [componentPage, setComponentPage] = useState(<DashBoard />);
@@ -20,6 +21,11 @@ export default Home = () => {
       <button onClick={() => setComponentPage(<AllKanji />)}>All Kanji</button>
       <button onClick={() => setComponentPage(<Quiz kanjiData={kanjiData} />)}>
         Quiz
+      </button>
+      <button
+        onClick={() => setComponentPage(<Practice kanjiData={kanjiData} />)}
+      >
+        Practice
       </button>
       <p>{componentPage}</p>
     </div>
