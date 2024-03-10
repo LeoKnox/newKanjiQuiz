0012d8f6f55a8341eb408a0a471635f214cb30a5
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-10 11:47:33 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import "./styles.css";
 import Home from "./components/Home.js";
 
 export default function App() {
-  const value = "kanji data";
   return (
     <div className="App">
       <Home />
