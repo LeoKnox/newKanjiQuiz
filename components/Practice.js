e9c5ec88e744ab57ad7eed18f5d7e9a369087a08
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-11 11:21:48 -0700

    Update Practice.js

--- components/Practice.js
+++ components/Practice.js
@@ -2,17 +2,25 @@ import { useState, useEffect } from "react";
 
 export default Practice = ({ kanjiData }) => {
   const [practiceKanji, setPracticeKanji] = useState(kanjiData);
+  const [position, setPosition] = useState(0);
   const [randomSet, setRandomSet] = useState(true);
   useEffect(() => {
     console.log("begin");
   });
+  const advance = (e) => {
+    if (e.target.name === "next") {
+      alert("next");
+    }
+  };
   return (
     <div>
       <h2>Practice Kanji</h2>
       <div>
         <button>Previous</button>
         <label>{practiceKanji[0]["word"]}</label>
-        <button>Next</button>
+        <button onClick={advance} name="next">
+          Next
+        </button>
       </div>
     </div>
   );
