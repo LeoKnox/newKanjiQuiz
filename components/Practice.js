42a792eb65d1043a4644729c6e704c71a2d5ebd3
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-11 11:13:33 -0700

    Update Practice.js

--- components/Practice.js
+++ components/Practice.js
@@ -1,4 +1,11 @@
+import { useState, useEffect } from "react";
+
 export default Practice = ({ kanjiData }) => {
+    const [practiceKanji, setPracticeKanji] = useState({kanjiData});
+    const [randomSet, setRandomSet] = useState(true);
+    useEffect(() => {
+        console.log("begin");
+    })
   return (
     <div>
       <h2>Practice Kanji</h2>
