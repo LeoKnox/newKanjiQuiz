da72aa89025587605400900dd2e8d8663f646cd0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-18 09:36:01 -0700

    Update DrawKanji.js

--- components/DrawKanji.js
+++ components/DrawKanji.js
@@ -1,3 +1,30 @@
+import { useState } from "react";
+
 export default DrawKanji = () => {
+  const [points, setPoints] = useState([]);
+
+  const handleMouseDown = (event) => {
+    const { clientX, clientY } = event;
+    setPoints([...points, { x: clientX, y: clientY }]);
+  };
+
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    setPoints([...points, { x: clientX, y: clientY }]);
+  };
+
+  const handleMouseUp = () => {
+    setPoints([]);
+  };
+
+  const renderPolyline = () => {
+    const context = Canvas.getContext("2d");
+
+    context.beginPath();
+    points.forEach((point) => {
+      context.lineTo(point.x, point.y);
+    });
+    context.stroke();
+  };
   return <p>draw here</p>;
 };
