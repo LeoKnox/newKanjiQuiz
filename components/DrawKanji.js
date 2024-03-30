ec794f7865d9b4ddfaf2f19ca531e79a2eafee6f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-30 11:25:24 -0700

    Update DrawKanji.js

--- components/DrawKanji.js
+++ components/DrawKanji.js
@@ -38,10 +38,9 @@ export default DrawKanji = ({ advance, randomSet }) => {
   const handleMouseUp = () => {
     //setPoints([]);
     let newKanji = kanji;
-    let x = (
-      "<polyline point= "+{stroke.map((point) => `${point.x},${point.y}`).join(" ")}
-      +"/>"
-    );
+    let x ="<polyline point= "
+    x.append({stroke.map((point) => `${point.x},${point.y}`).join(" ")})
+    x.append("/>")
     newKanji.push([x]);
     setKanji(newKanji);
     setStroke([]);
