02c90fa55d263f5fb890ecbb99781c376685ca51
LeoKnox <LeoKnox@users.noreply.github.com>
2024-03-31 12:25:47 -0700

    Update DrawKanji.js

--- components/DrawKanji.js
+++ components/DrawKanji.js
@@ -101,8 +101,9 @@ export default DrawKanji = ({ advance, randomSet }) => {
         hanging="100px"
         style={{ border: "1px solid black" }}
       >
-        kanji.map((line) =>
-        {<polyline points={line} stroke="black" strokeWidth="2" />})
+        {kanji.map((line) => {
+          <polyline points={line} stroke="black" strokeWidth="2" />;
+        })}
       </svg>
       <button onClick={advance} name="next">
         Next
