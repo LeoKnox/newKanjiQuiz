2ac4143f4999057e6e39a7d9982ae23d1c248ed1
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-04 13:24:46 -0700

    Create Kanjidb.js

---
+++ components/Kanjidb.js
@@ -0,0 +1,14 @@
+export const senKanji = [
+  { word: "ichi", meaning: "one", kanji: "一" },
+  { word: "ni", meaning: "two", kanji: "二" },
+  { word: "san", meaning: "three", kanji: "三" },
+  { word: "yon", meaning: "four", kanji: "四" },
+  { word: "go", meaning: "five", kanji: "五" },
+  { word: "roku", meaning: "six", kanji: "六" },
+  { word: "hi", meaning: "fire, light", kanji: "火" },
+  { word: "hidari", meaning: "left", kanji: "左" },
+  { word: "migi", meaning: "right", kanji: "右" },
+  { word: "taka, takai", meaning: "high, costly, tall", kanji: "高" },
+  { word: "chiisai", meaning: "small, little", kanji: "小" },
+  { word: "ookii, TAI, DAI", meaning: "big, large, great", kanji: "大" },
+];
