d31ed60a0a7c7562acce3f62619f3df9cfa7dc11
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-06 14:13:30 -0700

    Update AllKanji.js

--- components/AllKanji.js
+++ components/AllKanji.js
@@ -2,10 +2,15 @@ import { kanjidb } from "./Kanjidb.js";
 
 export default AllKanji = () => {
   console.log("red");
-  console.log(kanjidb);
+  //console.log(kanjidb);
   return (
     <div>
       <h2>All Kanji Page</h2>
+      {kanjidb.map((kanji) => (
+        <p>
+          {kanji.word}:{kanji.meaning}:{kanji.kanji}
+        </p>
+      ))}
     </div>
   );
 };
