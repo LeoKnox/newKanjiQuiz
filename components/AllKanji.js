426621827c1efe56306539e93403a768c113d3d6
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-06 14:34:21 -0700

    Update AllKanji.js

--- components/AllKanji.js
+++ components/AllKanji.js
@@ -1,14 +1,16 @@
 import { kanjidb } from "./Kanjidb.js";
 
 export default AllKanji = () => {
-  console.log("red");
+  const updateKanji = () => {
+    console.log("update");
+  };
   //console.log(kanjidb);
   return (
     <div>
       <h2>All Kanji Page</h2>
       {kanjidb.map((kanji, index) => (
         <p>
-          <input type="checkbox" />
+          <input type="checkbox" onChange={() => updateKanji()} />
           {index} - {kanji.word}:{kanji.meaning}:{kanji.kanji}
         </p>
       ))}
