c890c9867c346433ff4b06ae79b03fedea0717a4
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-15 13:15:47 -0700

    Update DashBoard.js

--- components/DashBoard.js
+++ components/DashBoard.js
@@ -1,11 +1,19 @@
 export default DashBoard = ({ myKanji }) => {
+  kanjiTable = () => {
+    return (
+      <p>red</p>
+    )
+  }
   return (
     <div>
       <h2>Home Page</h2>
-      {myKanji.length === 0 ? (
+      {myKanji.length <= 0 ? (
         <p>No Kanji</p>
       ) : (
-        myKanji.map((kanji) => <p>{kanji.word}</p>)
+
+        myKanji.map((kanji) => <p>{kanji.word}</p>
+
+        )
       )}
     </div>
   );
