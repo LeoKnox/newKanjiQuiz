48d30256322e4e9511572a18c8586e0e02003a9a
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-29 13:07:11 -0700

    Update Quiz.js

--- components/Quiz.js
+++ components/Quiz.js
@@ -2,7 +2,55 @@ import { useState, useEffect } from "react";
 export default Quiz = ({ myKanji }) => {
   const [quizes, setQuizes] = useState([]);
   const [answer, setAnswer] = useState("");
+  const [showMeaning, setShowMeaning] = useState(false);import { useState, useEffect } from "react";
+export default Quiz = ({ myKanji }) => {
+  const [quizes, setQuizes] = useState([]);
+  const [answer, setAnswer] = useState("");
   const [showMeaning, setShowMeaning] = useState(false);
+  const [correct, setCorrect] = useState(Array(6).fill("black"));
+  useEffect(() => {
+    const shuffled = myKanji.sort(() => 0.5 - Math.random());
+    console.log(`shuffled ${JSON.stringify(shuffled)}`);
+    let selected = shuffled.slice(0, 6);
+    let randAnswer = selected[Math.floor(Math.random() * selected.length)];
+    setQuizes(selected);
+    setAnswer(randAnswer);
+  }, [answer]);
+  const confirmAnswer = (selection, index) => {
+    if (selection === answer.meaning) {
+      setCorrect(correct.map((value, i) => (i === index ? "green" : value)));
+      setShowMeaning(!showMeaning);
+      setTimeout(() => {
+        setAnswer("correct");
+        setCorrect(Array(6).fill("black"));
+      }, 2000);
+    } else {
+      setCorrect(correct.map((value, i) => (i === index ? "red" : value)));
+    }
+  };
+  return (
+    <div>
+      <h2>Kanji Quiz Page</h2>
+      <p>answer {showMeaning ? answer.meaning : answer.word}</p>
+      <button onClick={() => setShowMeaning(!showMeaning)}>
+        {showMeaning ? "Word" : "Meaning"}
+      </button>
+      <div className="quizLayout">
+        {quizes.map((kanji, index) => (
+          <>
+            <button
+              style={{ borderColor: correct[index] }}
+              onClick={() => confirmAnswer(kanji.meaning, index)}
+            >
+              {kanji.kanji}
+            </button>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
   const [correct, setCorrect] = useState(Array(6).fill("black"));
   useEffect(() => {
     const shuffled = myKanji.sort(() => 0.5 - Math.random());
