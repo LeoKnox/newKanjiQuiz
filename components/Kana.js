a7a1acfec923170bb7dc676063f59c5487a93934
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-07 16:29:34 -0700

    Update Kana.js

--- components/Kana.js
+++ components/Kana.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 export default Kana = () => {
   const [kana, setKana] = useState("hiragana");
+  const changeKana = () => {
+    kana == "hiragana" ? setKana("kataKana") : setKana("hiragarana");
+  };
 
-  return;
-  <div>
-    <h3>Practice Kana</h3>
-    <button>{kana}</button>
-  </div>;
+  return (
+    <div>
+      <h3>Practice Kana</h3>
+      <button onClick={changeKana}>{kana}</button>
+    </div>
+  );
 };
