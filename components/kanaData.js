2fc377700068be77d678d5d4ef3c8327908c48b5
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-08 15:24:32 -0700

    Update kanaData.js

--- components/kanaData.js
+++ components/kanaData.js
@@ -1,14 +1,3 @@
-/*あ	い	う	え	お
-k	か	き	く	け	こ
-s	さ	し	す	せ	そ
-t	た	ち	つ	て	と
-n	な	に	ぬ	ね	の
-h	は	ひ	ふ	へ	ほ
-m	ま	み	む	め	も
-y	や	[6]	ゆ	[6]	よ
-r	ら	り	る	れ	ろ
-w	わ	ゐ	[6]	ゑ	を
-ん (n)*/
 export const hiraganaData = [
   { sound: "a", kana: "あ" },
   { sound: "i", kana: "い" },
