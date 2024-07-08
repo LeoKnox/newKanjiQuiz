516b0e337e224830c058c40bc60ea85ed2fd5f20
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-08 14:29:02 -0700

    Update kanaData.js

--- components/kanaData.js
+++ components/kanaData.js
@@ -1,12 +1,25 @@
 export const hiraganaData = [
-    {soud:"a", kana:"あ"},
-    {soud:"i", kana:"い"},
-    {soud:"u", kana:"う"},
-    {soud:"e", kana:"え"},
-    {soud:"o", kana:"お"},
-    {soud:"ka", kana:"か"},
-    {soud:"ki", kana:"き"},
-    {soud:"ku", kana:"く"},
-    {soud:"ke", kana:"け"},
-    {soud:"ko", kana:"こ"},
-]
+  { soud: "a", kana: "あ" },
+  { soud: "i", kana: "い" },
+  { soud: "u", kana: "う" },
+  { soud: "e", kana: "え" },
+  { soud: "o", kana: "お" },
+  { soud: "ka", kana: "か" },
+  { soud: "ki", kana: "き" },
+  { soud: "ku", kana: "く" },
+  { soud: "ke", kana: "け" },
+  { soud: "ko", kana: "こ" },
+];
+
+export const katakanaData = [
+  { soud: "a", kana: "ア" },
+  { soud: "i", kana: "イ" },
+  { soud: "u", kana: "ウ" },
+  { soud: "e", kana: "エ" },
+  { soud: "o", kana: "オ" },
+  { soud: "ka", kana: "カ" },
+  { soud: "ki", kana: "キ" },
+  { soud: "ku", kana: "ク" },
+  { soud: "ke", kana: "ケ" },
+  { soud: "ko", kana: "コ" },
+];
