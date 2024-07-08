7d322e5d07c3c9ac2333eb0f39dafb36ccc17e53
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-08 15:23:49 -0700

    Update Kana.js

--- components/Kana.js
+++ components/Kana.js
@@ -3,7 +3,7 @@ import { hiraganaData, katakanaData } from "./kanaData.js";
 
 export default Kana = () => {
   const [kana, setKana] = useState(false);
-
+  const shuffled = hiraganaData.sort(() => 0.5 - Math.random());
   return (
     <div>
       <h3>Practice Kana</h3>
@@ -13,7 +13,12 @@ export default Kana = () => {
       {kana ? (
         <p>kata {katakanaData[1].kana}</p>
       ) : (
-        <p>hira {hiraganaData[0].kana}</p>
+        <p>
+          hira
+          {shuffled.map((tile) => {
+            <label>{tile.sound}</label>;
+          })}
+        </p>
       )}
     </div>
   );
