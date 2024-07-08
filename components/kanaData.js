8992ee1a5b5f3b1790d64d4655fe7ad01db3100f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-08 15:24:17 -0700

    Update kanaData.js

--- components/kanaData.js
+++ components/kanaData.js
@@ -1,25 +1,36 @@
+/*あ	い	う	え	お
+k	か	き	く	け	こ
+s	さ	し	す	せ	そ
+t	た	ち	つ	て	と
+n	な	に	ぬ	ね	の
+h	は	ひ	ふ	へ	ほ
+m	ま	み	む	め	も
+y	や	[6]	ゆ	[6]	よ
+r	ら	り	る	れ	ろ
+w	わ	ゐ	[6]	ゑ	を
+ん (n)*/
 export const hiraganaData = [
-  { soud: "a", kana: "あ" },
-  { soud: "i", kana: "い" },
-  { soud: "u", kana: "う" },
-  { soud: "e", kana: "え" },
-  { soud: "o", kana: "お" },
-  { soud: "ka", kana: "か" },
-  { soud: "ki", kana: "き" },
-  { soud: "ku", kana: "く" },
-  { soud: "ke", kana: "け" },
-  { soud: "ko", kana: "こ" },
+  { sound: "a", kana: "あ" },
+  { sound: "i", kana: "い" },
+  { sound: "u", kana: "う" },
+  { sound: "e", kana: "え" },
+  { sound: "o", kana: "お" },
+  { sound: "ka", kana: "か" },
+  { sound: "ki", kana: "き" },
+  { sound: "ku", kana: "く" },
+  { sound: "ke", kana: "け" },
+  { sound: "ko", kana: "こ" },
 ];
 
 export const katakanaData = [
-  { soud: "a", kana: "ア" },
-  { soud: "i", kana: "イ" },
-  { soud: "u", kana: "ウ" },
-  { soud: "e", kana: "エ" },
-  { soud: "o", kana: "オ" },
-  { soud: "ka", kana: "カ" },
-  { soud: "ki", kana: "キ" },
-  { soud: "ku", kana: "ク" },
-  { soud: "ke", kana: "ケ" },
-  { soud: "ko", kana: "コ" },
+  { sound: "a", kana: "ア" },
+  { sound: "i", kana: "イ" },
+  { sound: "u", kana: "ウ" },
+  { sound: "e", kana: "エ" },
+  { sound: "o", kana: "オ" },
+  { sound: "ka", kana: "カ" },
+  { sound: "ki", kana: "キ" },
+  { sound: "ku", kana: "ク" },
+  { sound: "ke", kana: "ケ" },
+  { sound: "ko", kana: "コ" },
 ];
