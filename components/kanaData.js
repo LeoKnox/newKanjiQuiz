5b0d7e608a9fe085c7db8faece523d6be0c65a1d
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-08 12:38:31 -0700

    Create kanaData.js

---
+++ components/kanaData.js
@@ -0,0 +1,12 @@
+export const hiraganaData = [
+    {soud:"a", kana:"あ"},
+    {soud:"i", kana:"い"},
+    {soud:"u", kana:"う"},
+    {soud:"e", kana:"え"},
+    {soud:"o", kana:"お"},
+    {soud:"ka", kana:"か"},
+    {soud:"ki", kana:"き"},
+    {soud:"ku", kana:"く"},
+    {soud:"ke", kana:"け"},
+    {soud:"ko", kana:"こ"},
+]
