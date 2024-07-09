df4f7595e23980eb989b163750936848262566b0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-09 13:07:17 -0700

    Update Kana.js

--- components/Kana.js
+++ components/Kana.js
@@ -3,46 +3,23 @@ import { hiraganaData, katakanaData } from "./kanaData.js";
 
 export default Kana = () => {
   const [kana, setKana] = useState(false);
-  const shuffled = hiraganaData.sort(() => 0.5 - Math.random()).slice(0, 5);
-  console.log(shuffled);
-  return (
-    <div>
-      <h3>Practice Kana</h3>
-      <button onClick={() => setKana(!kana)}>
-        {kana ? "kataKana" : "hiragarana"}
-      </button>
-      {kana ? (
-        <p>kata {katakanaData[1].kana}</p>
-      ) : (
-        <p className="kanaTest">
-          hira
-          {shuffled.map((tile) => (
-            <label>:{tile.sound}</label>
-          ))}
-        </p>
-      )}
-    </div>
-  );
-};
-import { useState } from "react";
-import { hiraganaData, katakanaData } from "./kanaData.js";
-
-export default Kana = () => {
-  const [kana, setKana] = useState(false);
   const shuffled = hiraganaData.sort(() => 0.5 - Math.random()).slice(0, 6);
+  const answer = shuffled[Math.floor(Math.random() * shuffled.length)];
   return (
     <div>
       <h3>Practice Kana</h3>
+
       <button onClick={() => setKana(!kana)}>
         {kana ? "kataKana" : "hiragarana"}
       </button>
+      <p>{answer.sound}</p>
       {kana ? (
         <p>kata {katakanaData[1].kana}</p>
       ) : (
         <p className="kanaTest">
           {shuffled.map((tile) => (
             <button>
-              <label>{tile.sound}</label>
+              <label>{tile.kana}</label>
             </button>
           ))}
         </p>
