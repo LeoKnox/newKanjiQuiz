b77341ba9ca2cc106199a4a682926b37db67be45
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-09 13:02:17 -0700

    Update Kana.js

--- components/Kana.js
+++ components/Kana.js
@@ -24,3 +24,29 @@ export default Kana = () => {
     </div>
   );
 };
+import { useState } from "react";
+import { hiraganaData, katakanaData } from "./kanaData.js";
+
+export default Kana = () => {
+  const [kana, setKana] = useState(false);
+  const shuffled = hiraganaData.sort(() => 0.5 - Math.random()).slice(0, 6);
+  return (
+    <div>
+      <h3>Practice Kana</h3>
+      <button onClick={() => setKana(!kana)}>
+        {kana ? "kataKana" : "hiragarana"}
+      </button>
+      {kana ? (
+        <p>kata {katakanaData[1].kana}</p>
+      ) : (
+        <p className="kanaTest">
+          {shuffled.map((tile) => (
+            <button>
+              <label>{tile.sound}</label>
+            </button>
+          ))}
+        </p>
+      )}
+    </div>
+  );
+};
