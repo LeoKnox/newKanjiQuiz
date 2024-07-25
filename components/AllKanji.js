3f180e8e3f9d3ba47f48030506b9bfb763d90ddf
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-25 15:41:27 -0700

    Update AllKanji.js

--- components/AllKanji.js
+++ components/AllKanji.js
@@ -5,8 +5,7 @@ import { kanjidb2 } from "./kanjidb_L2.js";
 export default AllKanji = ({ myKanji, setMyKanji }) => {
   const testArray = [kanjidb1, kanjidb2];
   const [grades, setGrades] = useState([
-    1,
-    //Array.from(new Set(myKanji.map((item) => item.grade))),
+    Array.from(new Set(myKanji.map((item) => item))),
   ]);
   const [kanjidb, setKanjidb] = useState([]);
   let checkedKanji = myKanji;
