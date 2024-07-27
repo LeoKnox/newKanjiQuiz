a2d9611f1a18c9edb9064c8a536d33b914d1a5c9
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-27 12:46:37 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,45 +1,50 @@
-import { useState } from "react";
-import { KanjiContext } from "../KanjiContext.js";
-import DashBoard from "./DashBoard.js";
-import AllKanji from "./AllKanji.js";
-import Quiz from "./Quiz.js";
-import Practice from "./Practice.js";
-import Kana from "./Kana.js";
+import { useState, useEffect } from "react";
+import FloorMap from "./FloorMap.js";
+import GirdMap from "./GridMap.js";
 
 export default Home = () => {
-  const [myKanji, setMyKanji] = useState([]);
-  const [componentPage, setComponentPage] = useState(
-    <DashBoard myKanji={myKanji} />
-  );
+  const [columns, setColumns] = useState(3);
+  const [test, setTest] = useState([1, 2, 3, 4, 5, 6]);
+  const [floor, setFloor] = useState();
+  console.log({ setFloor });
 
+  const gridStyle = {
+    color: "blue",
+    gridTemplateColumns: `repeat(${columns}, 1fr)`,
+    //gridTemplateRows: `repeat(${columns}, 1fr)`,
+  };
+  const playersState = [
+    { id: 1, gridArea: "4/5", hp: 18, type: "user", token: "赤", name: "Aka" },
+    { id: 2, gridArea: "4/4", hp: 18, type: "user", token: "青", name: "Ao" },
+    {
+      id: 3,
+      gridArea: "5/5",
+      hp: 18,
+      type: "user",
+      token: "緑",
+      name: "Midori",
+    },
+    {
+      id: 4,
+      gridArea: "5/4",
+      hp: 18,
+      type: "user",
+      token: "紫",
+      name: "Murasaki",
+    },
+  ];
   return (
     <div>
-      <button onClick={() => setComponentPage(<DashBoard myKanji={myKanji} />)}>
-        Home
-      </button>
-      <button
-        onClick={() =>
-          setComponentPage(
-            <AllKanji myKanji={myKanji} setMyKanji={setMyKanji} />
-          )
-        }
-      >
-        All Kanji
-      </button>
-      <button
-        disabled={myKanji.length < 10}
-        onClick={() => setComponentPage(<Quiz myKanji={myKanji} />)}
-      >
-        Quiz
-      </button>
-      <button onClick={() => setComponentPage(<Practice myKanji={myKanji} />)}>
-        Practice
-      </button>
-      <button onClick={() => setComponentPage(<Kana />)}>Kana</button>
-      <p hidden={myKanji.length >= 10}>
-        10 Kanji needed for quiz you have {myKanji.length}
-      </p>
-      <p>{componentPage}</p>
+      <h3>Home Page</h3>
+      {columns ? (
+        <GridMap />
+      ) : (
+        <div className="column" style={gridStyle}>
+          {test.map((t) => (
+            <div id={t % test.length}>{t}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
