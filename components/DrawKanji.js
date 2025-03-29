a2efe205dc61e26fe928cd3969381d38e3f171fd
LeoKnox <LeoKnox@users.noreply.github.com>
2025-03-28 18:13:35 -0700

    Update DrawKanji.js

--- components/DrawKanji.js
+++ components/DrawKanji.js
@@ -1,134 +1,110 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, cloneElement } from "react";
+import { demonSVG, floorSVG, wallSVG, warriorSVG, warr } from "./svgData";
+import { singleRoom } from "./dungeonData.js";
+import { charLocation, updateMonster, mobData, mapData } from "./playData.js";
+import DrawMonster from "./DrawMonster.js";
+import DrawCharacter from "./DrawCharacter.js";
 
-export default DrawKanji = ({
-  guide,
-  advance,
-  randomSet,
-  clean,
-  showGuide,
+export default DrawMap = ({
+  width = 10,
+  height = 10,
+  x = 2,
+  y = 3,
+  sety,
+  setx,
 }) => {
-  const [stroke, setStroke] = useState([]);
-  const [kanji, setKanji] = useState([]);
-  const [draw, setDraw] = useState(false);
-  console.log(`show guide ${showGuide}`);
+  const currMap = new Array(singleRoom().x)
+    .fill()
+    .map(() => <tr>{Array(singleRoom().y).fill(<td>{floorSVG()}</td>)}</tr>);
+  const [dataMove, setDataMove] = useState(currMap);
+  const [locations, setLocations] = useState(mapData);
+  const [char, setChar] = useState(charLocation());
+  const [mapState, setMapState] = useState(() => {
+    let temp = [];
+    let tempRow = [];
+    for (let x = 0; x < width + 2; x++) {
+      tempRow.push(wallSVG());
+    }
+    temp.push(tempRow);
+    tempRow = [];
+    for (let x = 0; x < height; x++) {
+      tempRow.push(wallSVG());
+      for (let y = 0; y < width; y++) {
+        tempRow.push(floorSVG());
+      }
+      tempRow.push(wallSVG());
+      temp.push(tempRow);
+      tempRow = [];
+    }
+    for (let x = 0; x < width + 2; x++) {
+      tempRow.push(wallSVG());
+    }
+    temp.push(tempRow);
+    tempRow = [];
+    return temp;
+  });
 
   useEffect(() => {
-    clearPractice();
-  }, [clean, advance]);
+    console.log("locations");
+    let temp = [...currMap];
+    temp[locations["player"].y][locations["player"].x] = warriorSVG();
+    locations["monster"].map((mon) => (temp[mon.y][mon.x] = demonSVG()));
+    setDataMove(temp);
+  }, [locations]);
 
-  const clearPractice = () => {
-    setKanji([]);
+  const moveMob = (xval = -1) => {
+    console.log("move mob");
+    let change = updateMonster(0, -1);
+    modifyTable(change);
   };
 
-  const handleMouseDown = (event) => {
-    const { clientX, clientY } = event;
-    setDraw(true);
+  const newMove = (x, y) => {
+    let temp = locations;
+    temp["player"].y = temp["player"].y + y;
+    temp["player"].x = temp["player"].x + x;
+    setLocations({ ...temp });
+    console.log("red");
+    console.log(dataMove);
   };
-
-  const handleMouseMove = (event) => {
-    const { clientX, clientY } = event;
-    let offset = document.getElementById("svg").getBoundingClientRect();
-    if (draw) {
-      setStroke([
-        ...stroke,
-        { x: clientX - offset.left, y: clientY - offset.top },
-      ]);
-    }
+  const confirm = (v) => {
+    console.log("CC");
+    console.log(v);
+    //let mmm = [...v]
+    v.props.children = wallSVG();
+    return v;
   };
-
-  const handleMouseUp = () => {
-    let newKanji = kanji;
-    let line = stroke.map((point) => `${point.x},${point.y}`).join(" ");
-    newKanji.push([line]);
-    setKanji(newKanji);
-    setStroke([]);
-    setDraw(false);
+  const createWalls = () => {
+    console.log("create walls");
+    let cw = [...dataMove, ((s) => [...s])];
+    let elements = dataMove.map((s, t) =>
+      t == locations.player.y
+        ? s.props.children.map((u, v) =>
+            v == locations.player.x ? warriorSVG() : u
+          )
+        : s
+    );
+    cw[locations.player.x][locations.player.y] = wallSVG();
+    return cw;
   };
-  const drawKanji = () => {
-    let testData = [];
-    kanji.map((x) => {
-      testData.push(
-        <polyline
-          points={x}
-          stroke="black"
-          strokeWidth="4"
-          fill="none"
-          zIndex="50"
-        />
-      );
-    });
-    return testData;
-  };
-
-  const drawLine = (
-    <polyline
-      points={stroke.map((point) => `${point.x},${point.y}`).join(" ")}
-      stroke="black"
-      strokeWidth="4"
-      fill="none"
-      zIndex="50"
-    />
-  );
 
   return (
-    <>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <button
-          className="moveKanji"
-          onClick={advance}
-          name="previous"
-          disabled={randomSet}
-          style={{ float: "left" }}
-        >
-          Previous
-        </button>
-
-        <div
-          style={{
-            position: "absolute",
-            width: "80px",
-            color: "lightgray",
-            fontSize: "5em",
-            visibility: showGuide ? "visible" : "hidden",
-          }}
-        >
-          {guide}
-        </div>
-        <div
-          onTouchStart={handleMouseDown}
-          onTouchMove={handleMouseMove}
-          onTouchEnd={handleMouseUp}
-        >
-          <svg
-            id="svg"
-            key="svg"
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}
-            width="109px"
-            hanging="100px"
-            style={{
-              border: "1px solid black",
-              position: "relative",
-              zIndex: "10",
-            }}
-          >
-            {drawLine}
-            {drawKanji()}
-          </svg>
-        </div>
-        <button
-          className="moveKanji"
-          onClick={advance}
-          name="next"
-          style={{ float: "right" }}
-        >
-          Next
-        </button>
-      </div>
-      <p>
-        <button onClick={clearPractice}>Clear</button>
-      </p>
-    </>
+    <div>
+      <label>
+        <button onClick={() => newMove(1, 0)}>right</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(-1, 0)}>left</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(0, 1)}>down</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(0, -1)}>up</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(0, 0)}>mob</button>
+      </label>
+      <table>{createWalls()}</table>
+    </div>
   );
 };
